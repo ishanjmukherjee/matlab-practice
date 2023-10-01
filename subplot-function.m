d8d04d4d4209d761185d812615b6f845c59e14f1
Ishan Mukherjee <ishanjmukherjee@gmail.com>
2023-10-02 02:13:48 +0530

    Update subplot-function.m
    Added explanation for interval choice of x, specifically why the interval must be a multiple of pi.

--- subplot-function.m
+++ subplot-function.m
@@ -3,6 +3,15 @@
 % Use the subplot function to plot sin(x) & abs(sin(x)) and cos(x) & 
 % abs(cos(x) in the same Figure Window.
 
+% Why have I chosen a multiple of pi (specifically, pi/1000) as the 
+% interval instead of something like 0.001? The former ensures 0 is in x, 
+% which results in a discontinuity while plotting cos(x)/x (since cos(0)/0 
+% is not defined, MATLAB doesn't plot it). This discontunity avoids a 
+% vertical-seeming line at x = 0 (connecting the points 
+% (x-ε,cos(x-ε)/(x-ε)) and (x+ε,cos(x+ε)/(x+ε)) that is seen if x =
+% -pi:0.001:pi instead, for example. Note that 0 won't be in x if the
+% interval is not a multiple of pi.
+
 x = -pi:pi/1000:pi;
 
 subplot(2,1,1)
