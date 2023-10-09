5da8625388f7d8b30843dc4caaaca29169a1b5bb
Ishan Mukherjee <ishanjmukherjee@gmail.com>
2023-10-09 07:26:01 +0530

    Create mean-and-stddev-while-loop.m

---
+++ mean-and-stddev-while-loop.m
@@ -0,0 +1,30 @@
+% EoMP Example 5.1
+% Problem statement:
+% Read in a set of user-inputted measurements and calculate the mean and
+% standard deviation of the data set.
+
+% Initializing variables.
+n = 0;
+sum_x = 0;
+sum_x2 = 0;
+
+x = input("Enter first value: ");
+
+% Receives input as long as input is non-numeric.
+fprintf("Enter anything non-numeric to terminate input process\n")
+while isnumeric(x)
+    n = n + 1;
+    % Adding sum(inputs) to sum variable in case user inputs a numeric 
+    % array.
+    sum_x = sum_x + sum(x);
+    sum_x2 = sum_x2 + sum(x.^2);
+end
+
+% Calculating mean and standard deviation.
+mean = sum_x / n;
+std_dev = sqrt((n*sum_x2 - sum_x^2)/(n * (n-1)));
+
+% Printing number of datapoints, mean and standard deviation.
+fprintf("The number of data points is %f\n", n);
+fprintf("The mean is %f\n", mean);
+fprintf("The standard deviation is %f", std_dev);
