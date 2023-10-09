ad2d466c11ba0f394342be0421917b83037f4e7c
Ishan Mukherjee <ishanjmukherjee@gmail.com>
2023-10-09 08:02:45 +0530

    Update mean-and-stddev-while-loop.m
    Changed method for terminating input process, and added functionality to handle cases where user inputs an array.

--- mean-and-stddev-while-loop.m
+++ mean-and-stddev-while-loop.m
@@ -10,21 +10,26 @@ sum_x2 = 0;
 
 x = input("Enter first value: ");
 
-% Receives input as long as input is non-numeric.
-fprintf("Enter anything non-numeric to terminate input process\n")
-while isnumeric(x)
-    n = n + 1;
-    % Adding sum(inputs) to sum variable in case user inputs a numeric 
-    % array.
+% Receives input as long as input is nonempty (ie, not just a return).
+fprintf("Hit return to terminate input process\n")
+while ~isempty(x) 
+    % Adding sum(inputs) to sum variable and length(x) to n in case user 
+    % inputs an array with >1 elements.
+    n = n + length(x);
     sum_x = sum_x + sum(x);
     sum_x2 = sum_x2 + sum(x.^2);
+    x = input("Enter next value: ");
 end
 
-% Calculating mean and standard deviation.
-mean = sum_x / n;
-std_dev = sqrt((n*sum_x2 - sum_x^2)/(n * (n-1)));
-
-% Printing number of datapoints, mean and standard deviation.
-fprintf("The number of data points is %f\n", n);
-fprintf("The mean is %f\n", mean);
-fprintf("The standard deviation is %f", std_dev);
+if n >= 2
+    % Calculating mean and standard deviation.
+    mean = sum_x / n;
+    std_dev = sqrt((n*sum_x2 - sum_x^2)/(n * (n-1)));
+    
+    % Printing number of datapoints, mean and standard deviation.
+    fprintf("The number of data points is %f\n", n);
+    fprintf("The mean is %f\n", mean);
+    fprintf("The standard deviation is %f\n", std_dev);
+else
+    fprintf("Not enough information.\n");
+end
